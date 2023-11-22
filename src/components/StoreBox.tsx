992855a7423a8a0502e7f23344801f2b0f078dd8
kimsooin77 <sm6784@naver.com>
2023-11-22 16:08:34 +0900

    클릭한 식당 id의 데이터만 가져와 상세 페이지 연결

--- src/components/StoreBox.tsx
+++ src/components/StoreBox.tsx
@@ -1,60 +1,74 @@
 import { Dispatch, SetStateAction } from "react";
-import { AiOutlineClose, AiOutlineInfoCircle, AiOutlineCheck, AiOutlinePhone } from "react-icons/ai";
+import {
+  AiOutlineClose,
+  AiOutlineInfoCircle,
+  AiOutlineCheck,
+  AiOutlinePhone,
+} from "react-icons/ai";
 import { HiOutlineMapPin } from "react-icons/hi2";
 import Image from "next/image";
 import { StoreType } from "@/interface";
+import { useRouter } from "next/router";
 
 interface StoreBoxProps {
-    store: StoreType | null;
-    setStore: Dispatch<SetStateAction<any>>;
+  store: StoreType | null;
+  setStore: Dispatch<SetStateAction<any>>;
 }
 
 export default function StoreBox({ store, setStore }: StoreBoxProps) {
-    return (
-        <div className="fixed trannsition easy-in-out delay-150 inset-x-0 mx-auto bottom-20 rounded-lg shadow-lg max-w-sm md:max-w-xl z-10 w-full bg-white">
-            {store && (
-            <>
-                <div className="p-8">
-                    <div className="flex justify-between items-start">
-                        <div className="flex gap-4 items-center">
-                            <Image src={store?.category
-                                ? `/image/makers/${store?.category}.png`
-                                : "/image/makers/default.png"}
-                                width={40}
-                                height={40 }
-                                alt="아이콘 이미지"
-                            />
-                            <div>
-                                <div className="font-semibold">{store?.name}</div>
-                                <div className="text-sm">{store?.storeType}</div>
-                            </div>
-                        </div>
-                        <button type="button" onClick={() => setStore(null)}>
-                            <AiOutlineClose />
-                        </button>
-                    </div>
-                    <div className="mt-2 flex gap-2 items-center">
-                        <HiOutlineMapPin />
-                        {store?.address}
-                    </div>
-                    <div className="mt-2 flex gap-2 items-center">
-                        <AiOutlinePhone />
-                        {store?.phone}
-                    </div>
-                    <div className="mt-2 flex gap-2 items-center">
-                        <AiOutlineInfoCircle />
-                        {store?.storeType}
-                    </div>
-                    <div className="mt-2 flex gap-2 items-center">
-                        <AiOutlineCheck />
-                        {store?.category}
-                    </div>
+  const router = useRouter();
+  return (
+    <div className="fixed trannsition easy-in-out delay-150 inset-x-0 mx-auto bottom-20 rounded-lg shadow-lg max-w-sm md:max-w-xl z-10 w-full bg-white">
+      {store && (
+        <>
+          <div className="p-8">
+            <div className="flex justify-between items-start">
+              <div className="flex gap-4 items-center">
+                <Image
+                  src={
+                    store?.category
+                      ? `/image/makers/${store?.category}.png`
+                      : "/image/makers/default.png"
+                  }
+                  width={40}
+                  height={40}
+                  alt="아이콘 이미지"
+                />
+                <div>
+                  <div className="font-semibold">{store?.name}</div>
+                  <div className="text-sm">{store?.storeType}</div>
                 </div>
-                <button type="button" onClick={() => window.alert("상세보기 작업중")} className="w-full bg-primary hover:bg-primaryHover focus:bg-primaryHover py-3 text-white font-semibold rounded-b-lg" >
-                    상세보기
-                </button>
-            </>
-            )}
-        </div>
-    )
+              </div>
+              <button type="button" onClick={() => setStore(null)}>
+                <AiOutlineClose />
+              </button>
+            </div>
+            <div className="mt-2 flex gap-2 items-center">
+              <HiOutlineMapPin />
+              {store?.address}
+            </div>
+            <div className="mt-2 flex gap-2 items-center">
+              <AiOutlinePhone />
+              {store?.phone}
+            </div>
+            <div className="mt-2 flex gap-2 items-center">
+              <AiOutlineInfoCircle />
+              {store?.storeType}
+            </div>
+            <div className="mt-2 flex gap-2 items-center">
+              <AiOutlineCheck />
+              {store?.category}
+            </div>
+          </div>
+          <button
+            type="button"
+            onClick={() => router.push(`/stores/${store.id}`)}
+            className="w-full bg-primary hover:bg-primaryHover focus:bg-primaryHover py-3 text-white font-semibold rounded-b-lg"
+          >
+            상세보기
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -1,12 +1,12 @@
-import { StoreApiResponse, StoreType } from '@/interface';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client';
+import { StoreApiResponse, StoreType } from "@/interface";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<StoreApiResponse | StoreType[]>
+  res: NextApiResponse<StoreApiResponse | StoreType[] | StoreType>
 ) {
-  const { page = "" }:{page?:string} = req.query;
+  const { page = "" }: { page?: string } = req.query;
   const prisma = new PrismaClient();
 
   if (page) {
@@ -14,22 +14,26 @@ export default async function handler(
     const skipPage = parseInt(page) - 1;
     const stores = await prisma.store.findMany({
       orderBy: { id: "asc" },
-      take: 10,  
-      skip : skipPage * 10
+      take: 10,
+      skip: skipPage * 10,
     }); // findMany 메서드를 통해 모든 record를 가져옴
-    
+
     res.status(200).json({
       page: parseInt(page),
       data: stores,
       totalCount: count,
-      totalPage : Math.ceil(count / 10),
+      totalPage: Math.ceil(count / 10),
     });
   } else {
+    const { id }: { id?: string } = req.query;
     const stores = await prisma.store.findMany({
       orderBy: { id: "asc" },
+      where: {
+        // 쿼리를 날릴 때 id가 있으면 id가 같은 데이터를 가져오고 없다면 where문을 무시할 수 있도록 빈 옵션 전달
+        id: id ? parseInt(id) : {},
+      },
     });
 
-    return res.status(200).json(stores);
+    return res.status(200).json(id ? stores[0] : stores);
   }
-
 }
