0b83f4a6b10f32ca8f11b32edf6d7809c687f0ee
kimsooin77 <sm6784@naver.com>
2023-11-22 16:47:44 +0900

    상세페이지에 지도와 마커 띄우기

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -12,30 +12,39 @@ declare global {
 const DEFAULT_LAT = 37.497625203;
 const DEFAULT_LNG = 127.03088379;
 
-interface MapProps{
-    setMap : Dispatch<SetStateAction<any>>
+const DEFAULT_ZOOM = 3;
+
+interface MapProps {
+  setMap: Dispatch<SetStateAction<any>>;
+  lat?: string | null;
+  lng?: string | null;
+  zoom?: number;
 }
 
-export default function Map({setMap}:MapProps) {
-    const loadKakaoMap = () => {
-        window.kakao.maps.load(() => {
-            const mapContainer = document.getElementById("map");
-            const mapOption = {
-                center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
-                level: 3
-            };
-            const map = new window.kakao.maps.Map(mapContainer, mapOption);
-            setMap(map);
-        });
-    };
-    return (
+export default function Map({ setMap, lat, lng, zoom }: MapProps) {
+  const loadKakaoMap = () => {
+    window.kakao.maps.load(() => {
+      const mapContainer = document.getElementById("map");
+      const mapOption = {
+        center: new window.kakao.maps.LatLng(
+          lat ?? DEFAULT_LAT,
+          lng ?? DEFAULT_LNG
+        ),
+        level: DEFAULT_ZOOM,
+      };
+      const map = new window.kakao.maps.Map(mapContainer, mapOption);
+      setMap(map);
+    });
+  };
+  return (
     <>
-        <Script
-            strategy="afterInteractive"
-            type="text/javascript"
-            src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT}&autoload=false`}
-            onReady={loadKakaoMap} />      
-        <div id="map" className="w-full h-screen"></div>      
+      <Script
+        strategy="afterInteractive"
+        type="text/javascript"
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT}&autoload=false`}
+        onReady={loadKakaoMap}
+      />
+      <div id="map" className="w-full h-screen"></div>
     </>
-    )
+  );
 }
