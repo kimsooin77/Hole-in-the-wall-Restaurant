3c4b75796abeba8152b12da3e9bd910821541b22
kimsooin77 <sm6784@naver.com>
2023-11-25 19:23:06 +0900

    맛집 등록 폼 생성

--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -15,35 +15,45 @@ export default async function handler(
 ) {
   const { page = "", limit = "", q, district }: ResponseType = req.query;
 
-  if (page) {
-    const count = await prisma.store.count();
-    const skipPage = parseInt(page) - 1;
-    const stores = await prisma.store.findMany({
-      orderBy: { id: "asc" },
-      where: {
-        name: q ? { contains: q } : {},
-        address: district ? { contains: district } : {},
-      },
-      take: parseInt(limit),
-      skip: skipPage * 10,
-    }); // findMany 메서드를 통해 모든 record를 가져옴
-
-    res.status(200).json({
-      page: parseInt(page),
-      data: stores,
-      totalCount: count,
-      totalPage: Math.ceil(count / 10),
+  if (req.method === "POST") {
+    // 데이터 생성을 처리한다.
+    const data = req.body;
+    const result = await prisma.store.create({
+      data: { ...data },
     });
+
+    return res.status(200).json(result);
   } else {
-    const { id }: { id?: string } = req.query;
-    const stores = await prisma.store.findMany({
-      orderBy: { id: "asc" },
-      where: {
-        // 쿼리를 날릴 때 id가 있으면 id가 같은 데이터를 가져오고 없다면 where문을 무시할 수 있도록 빈 옵션 전달
-        id: id ? parseInt(id) : {},
-      },
-    });
+    if (page) {
+      const count = await prisma.store.count();
+      const skipPage = parseInt(page) - 1;
+      const stores = await prisma.store.findMany({
+        orderBy: { id: "asc" },
+        where: {
+          name: q ? { contains: q } : {},
+          address: district ? { contains: district } : {},
+        },
+        take: parseInt(limit),
+        skip: skipPage * 10,
+      }); // findMany 메서드를 통해 모든 record를 가져옴
 
-    return res.status(200).json(id ? stores[0] : stores);
+      res.status(200).json({
+        page: parseInt(page),
+        data: stores,
+        totalCount: count,
+        totalPage: Math.ceil(count / 10),
+      });
+    } else {
+      const { id }: { id?: string } = req.query;
+      const stores = await prisma.store.findMany({
+        orderBy: { id: "asc" },
+        where: {
+          // 쿼리를 날릴 때 id가 있으면 id가 같은 데이터를 가져오고 없다면 where문을 무시할 수 있도록 빈 옵션 전달
+          id: id ? parseInt(id) : {},
+        },
+      });
+
+      return res.status(200).json(id ? stores[0] : stores);
+    }
   }
 }
